8f09694b6f35fdb52429ac7f8859665acca00fdd
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2026-06-25 18:43:15 +0700

    fix

--- index.js
+++ index.js
@@ -13,16 +13,31 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 3001;
 
+const allowedOrigins = process.env.CORS_ORIGINS.split(",");
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGINS.split(","),
+    origin: function (origin, callback) {
+      // allow tools like Postman / server-to-server
+      if (!origin) return callback(null, true);
+
+      const normalizedOrigin = origin.replace(/\/$/, ""); // remove trailing slash
+
+      if (allowedOrigins.includes(normalizedOrigin)) {
+        return callback(null, true);
+      }
+
+      console.log("CORS blocked origin:", origin);
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   }),
 );
-
+// app.options("*", cors());
 app.use(cookieParser());
 
+// console.log
+console.log("ORIGINS:", allowedOrigins);
 // Dùng thằng này để truy cập vào link ảnh: http://localhost:3001/uploads/filename.jpg
 const path = require("path");
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
